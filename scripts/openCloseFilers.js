5a90e0e6d9c8aedee2b5c2fda563d3a3fc811b7e
Paul-Adrien <paul-adrien-for-ever@hotmail.fr>
2022-12-11 23:02:37 +0100

    fix bug filters open and close

--- scripts/openCloseFilers.js
+++ scripts/openCloseFilers.js
@@ -1,5 +1,6 @@
 export function handleOpenCloseFilters() {
   const filters = document.querySelectorAll('.filter__selector');
+  const filtersInputs = document.querySelectorAll('.filter__input');
 
   filters[0].addEventListener('click', openCloseFilters);
   filters[1].addEventListener('click', openCloseFilters);
@@ -9,12 +10,18 @@ export function handleOpenCloseFilters() {
     if (this.classList.contains('open')) {
       this.classList.remove('open');
       filters.forEach((filter) => filter.classList.remove('open'));
-
-      const input = this.firstChild.nextElementSibling;
-      input.type = 'button';
-      input.value = input.dataset.value;
+      filtersInputs.forEach((input) => {
+        input.type = 'button';
+        input.value = input.dataset.value;
+      });
     } else {
       filters.forEach((filter) => filter.classList.remove('open'));
+
+      filtersInputs.forEach((input) => {
+        input.type = 'button';
+        input.value = input.dataset.value;
+      });
+
       this.classList.add('open');
 
       const input = this.firstChild.nextElementSibling;
