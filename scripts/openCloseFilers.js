f17d2e7026f82fd8c29f3916973351a4bcd530f7
Paul-Adrien <paul-adrien-for-ever@hotmail.fr>
2022-12-13 01:07:35 +0100

    fix filters with stop propagation

--- scripts/openCloseFilers.js
+++ scripts/openCloseFilers.js
@@ -1,32 +1,38 @@
 export function handleOpenCloseFilters() {
   const filters = document.querySelectorAll('.filter__selector');
   const filtersInputs = document.querySelectorAll('.filter__input');
+  const closeFilterBtn = document.querySelectorAll('.filter__arrow');
 
-  filters[0].addEventListener('click', openCloseFilters);
-  filters[1].addEventListener('click', openCloseFilters);
-  filters[2].addEventListener('click', openCloseFilters);
+  filters[0].addEventListener('click', openFilter);
+  filters[1].addEventListener('click', openFilter);
+  filters[2].addEventListener('click', openFilter);
 
-  function openCloseFilters() {
-    if (this.classList.contains('open')) {
-      this.classList.remove('open');
-      filters.forEach((filter) => filter.classList.remove('open'));
-      filtersInputs.forEach((input) => {
-        input.type = 'button';
-        input.value = input.dataset.value;
-      });
-    } else {
-      filters.forEach((filter) => filter.classList.remove('open'));
+  closeFilterBtn[0].addEventListener('click', closeFilter);
+  closeFilterBtn[1].addEventListener('click', closeFilter);
+  closeFilterBtn[2].addEventListener('click', closeFilter);
 
-      filtersInputs.forEach((input) => {
-        input.type = 'button';
-        input.value = input.dataset.value;
-      });
+  function openFilter() {
+    filters.forEach((filter) => filter.classList.remove('open'));
 
-      this.classList.add('open');
+    filtersInputs.forEach((input) => {
+      input.type = 'button';
+      input.value = input.dataset.value;
+    });
 
-      const input = this.firstChild.nextElementSibling;
-      input.type = 'text';
-      input.value = '';
-    }
+    this.classList.add('open');
+
+    const input = this.firstChild.nextElementSibling;
+    input.type = 'text';
+    input.value = '';
+  }
+
+  function closeFilter(e) {
+    e.stopPropagation();
+    filters.forEach((filter) => filter.classList.remove('open'));
+
+    filtersInputs.forEach((input) => {
+      input.type = 'button';
+      input.value = input.dataset.value;
+    });
   }
 }
