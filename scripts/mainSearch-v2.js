3cd3046641466b2d15d0185b165337bdff8a26c4
Paul-Adrien <paul-adrien-for-ever@hotmail.fr>
2023-02-08 19:27:02 +0100

    remove console log

--- scripts/mainSearch-v2.js
+++ scripts/mainSearch-v2.js
@@ -21,8 +21,6 @@ export function mainSearchV2(e) {
     ];
   }
 
-  console.log(searchedWords);
-
   if (e.target.value.length >= 3 || e.target.value.length === 0) {
     cardContainer.textContent = '';
 
